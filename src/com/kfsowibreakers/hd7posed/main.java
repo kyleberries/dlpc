8372e7992dd5e6f07a4892d5a023e3e69c164c6e
kyleberries <kyleberry.graphics@gmail.com>
2014-03-27 22:11:30 -0500

    Update main.java

--- src/com/kfsowibreakers/hd7posed/main.java
+++ src/com/kfsowibreakers/hd7posed/main.java
@@ -1,1 +1,11 @@
 package com.kfsowibreakers.hd7posed;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Tutorial implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}

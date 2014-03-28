a0787bee7fb2d185d6aa999bcd4d441e71183bd7
kyleberries <kyleberry.graphics@gmail.com>
2014-03-27 22:22:41 -0500

    Update main.java

--- src/com/kfsowibreakers/hd7posed/main.java
+++ src/com/kfsowibreakers/hd7posed/main.java
@@ -13,7 +13,25 @@ public class pkgcheck implements IXposedHookLoadPackage {
 
 //looks like it, just need to figure how to implement
 
-// DownloadProvider fix (play store, gmail... downloads)
+public void handleLoadPackage( final LoadPackageParam lpparam ) throws Throwable 
+    {
+		// prevent ota update
+		if( lpparam.packageName.equals( "com.android.settings" ) )
+    	{
+	        try
+			{
+	        	final Class<?> cls = findClass( "com.android.settings.services.SystemUpdatesService", lpparam.classLoader );
+	    		Method m = findMethodBestMatch( cls, "initOTAController" );
+	    		XposedBridge.hookMethod( m, XC_MethodReplacement.DO_NOTHING );
+	    		log( cls.getName()+"."+m.getName()+" hooked" );  
+			}
+	    	catch ( Throwable t ) 
+	    	{
+	    		log( t );
+	    	}
+    	}
+
+		// DownloadProvider fix (play store, gmail... downloads)
         // Prevent any call to DownloadProvider.checkInsertPermissions
 		if( lpparam.packageName.equals( "com.android.providers.downloads" ) )
     	{
@@ -29,3 +47,4 @@ public class pkgcheck implements IXposedHookLoadPackage {
 	    		log( t );
 	    	}
     	}
+	}
